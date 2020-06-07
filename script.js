0cf366aabc4211afb9d51ba55d784fd29abe9042
ugurduyar <ismailugurduyar@gmail.com>
2020-06-07 14:22:35 +0300

    play pause added

--- script.js
+++ script.js
@@ -29,7 +29,31 @@ function loadSong(song) {
   cover.src = `images/${song}.jpg`;
 }
 
+// Play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
 // Event listeners
 playBtn.addEventListener("click", () => {
   const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 });
